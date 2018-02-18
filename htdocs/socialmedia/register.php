e3c9c85e569aa42ea2e06584690097c2c405dc6b
Kesava <psyku@nottingham.ac.uk>
2018-02-18 17:49:10 +0000

    Handeling and protecting form

--- htdocs/socialmedia/register.php
+++ htdocs/socialmedia/register.php
@@ -16,6 +16,43 @@ $date = ""; //sign up date
 $error_array = ""; //Holds error messages
 
 
+if (isset($_POST['register_button'])){
+
+	//Registration form values
+
+	//First name
+	$fname = strip_tags($_POST['reg_fname']); //remove html tags
+	$fname = str_replace('', '', $fname); //remove spaces
+	$fname = ucfirst(strtolower($fname)); //Uppercase first letter
+	$_SESSION['reg_fname'] = $fname; //stores first name into session variable
+
+	//Last name
+	$lname = strip_tags($_POST['reg_lname']); //remove html tags
+	$lname = str_replace('', '', $lname); //remove spaces
+	$lname = ucfirst(strtolower($lname)); //Uppercase first letter
+	$_SESSION['reg_lname'] = $fname; //stores last name into session variable
+
+
+	//Email
+	$em = strip_tags($_POST['reg_email']); //remove html tags
+	$em = str_replace('', '', $em); //remove spaces
+	$em = ucfirst(strtolower($em)); //Uppercase first letter
+	$_SESSION['reg_email'] = $email; //stores email into session variable
+
+
+	//Email 2
+	$em2 = strip_tags($_POST['reg_email2']); //remove html tags
+	$em2 = str_replace('', '', $em2); //remove spaces
+	$em2 = ucfirst(strtolower($em2)); //Uppercase first letter
+	$_SESSION['reg_email2'] = $email2; //stores email2 into session variable
+
+
+	//Password
+	$password = strip_tags($_POST['reg_password']); //remove html tags
+	$password2 = strip_tags($_POST['reg_password2']); //remove html tags
+
+	$date = date ("Y-a-d"); //current date
+
 ?>
 
 <html>
