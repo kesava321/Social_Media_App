ea95064be35372397fe49a052f685250d376704d
Kesava <psyku@nottingham.ac.uk>
2018-02-22 23:33:41 +0000

    Added topbar

--- htdocs/socialmedia/includes/header.php
+++ htdocs/socialmedia/includes/header.php
@@ -5,7 +5,7 @@ if (isset($_SESSION['username'])){
 	$userLoggedIn = $_SESSION['username'];
 }
 else{
-	header("Location: register.php");
+	//header("Location: register.php"); //UNCOMMENT LATER
 }
 
 ?>
@@ -17,10 +17,18 @@ else{
 
 	<!-- Javascript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-	<script src="assets/js/bootstrap.js"></script>script> 
+	<script src="assets/js/bootstrap.js"></script>
 	<!-- allows reuse of existing symbols/buttons -->
 
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="assets/css/bottstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+
+	<div class="top_bar">
+		<div class="logo">
+			<a href="index.php">Social Group</a>
+		</div>
+
+	</div>
