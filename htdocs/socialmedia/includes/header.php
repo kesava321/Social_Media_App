70ef66acaa0542135592aa07e23915d9ca4ad66e
Kesava <psyku@nottingham.ac.uk>
2018-02-22 23:10:51 +0000

    Added Twitter Bootstrap

--- htdocs/socialmedia/includes/header.php
+++ htdocs/socialmedia/includes/header.php
@@ -6,13 +6,21 @@ if (isset($_SESSION['username'])){
 }
 else{
 	header("Location: register.php");
- ?>
-}
 }
 
-<!DOCTYPE html>
+?>
+
+
 <html>
 <head>
 	<title>Welcome to SocialGroup</title>
+
+	<!-- Javascript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>script> 
+	<!-- allows reuse of existing symbols/buttons -->
+
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bottstrap.css">
 </head>
 <body>
