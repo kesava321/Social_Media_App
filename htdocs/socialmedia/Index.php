7e03e2862c90a468a0ac29afad129538da6ab88a
kesava <psyku@nottingham.ac.uk>
2018-02-24 19:26:33 +0000

    Created User class

--- htdocs/socialmedia/Index.php
+++ htdocs/socialmedia/Index.php
@@ -1,11 +1,12 @@
 <?php 
 include("includes/header.php");
+include("includes/classes/User.php");
  ?>
  	<div class="user_details column">
- 		<a href="#"> <img src="<?php echo $user['profile_pic']; ?>"> </a>
+ 		<a href="<?php echo $userLoggedIn; ?>"> <img src="<?php echo $user['profile_pic']; ?>"> </a>
  		
  		<div class="user_details_left_right">
-	 		<a href="#">
+	 		<a href="<?php echo $userLoggedIn; ?>">
 			<?php 
 	 		echo $user['first_name'] . " " . $user['last_name'];
 
@@ -19,6 +20,22 @@ include("includes/header.php");
 		</div>
 
  	</div>
+
+	<div class="main_column column">
+		<form class="post_form" action="index.php" method="POST">
+			<textarea name="post_text" id="post_text" placeholder="What's on your mind?"></textarea>
+			<input type="submit" name="post" id="post_button" value="Post"> 
+			<br>
+
+		</form>
+		<?php
+		
+		$user_obj = new User($con, $userLoggedIn);
+		echo $user_obj->getFirstAndLastName();
+		
+		?>
+	</div>
+
 
 
 
