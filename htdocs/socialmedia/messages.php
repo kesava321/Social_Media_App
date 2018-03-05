2b3de1928fbf43fc32ab9687c3ce2625ee6cc45c
kesava <psyku@nottingham.ac.uk>
2018-03-05 16:40:46 +0000

    Get recent interactions of user

--- htdocs/socialmedia/messages.php
+++ htdocs/socialmedia/messages.php
@@ -9,6 +9,38 @@ else {
 	$user_to = $message_obj->getMostRecentUser(); //Retrieve most recent user that user has had an interaction with
 	if($user_to == false)
 		$user_to = 'new';
-
 }
+
+
+if($user_to != "new") //if user is not new, then create a new user object out of the username we have here 
+	$user_to_obj = new User($con, $user_to);
 ?>
+	
+	<div class="user_details column">
+ 		<a href="<?php echo $userLoggedIn; ?>"> <img src="<?php echo $user['profile_pic']; ?>"> </a>
+ 		
+ 		<div class="user_details_left_right">
+	 		<a href="<?php echo $userLoggedIn; ?>">
+			<?php 
+	 		echo $user['first_name'] . " " . $user['last_name'];
+
+	 		?> 
+			</a>
+			<br>
+			<?php echo "Posts: " . $user['num_posts']. "<br>"; 
+			echo "Likes: " . $user['num_likes'];
+			
+			?> 
+		</div>
+
+ 	</div>
+
+ 	<div class="main_column column" id="main_column">
+ 		<?php
+ 		if($user_to != "new")
+ 			echo "<h4>You and <a href='$user_to'>" . $user_to_obj->getFirstAndLastName() . "</a></h4><hr><br>";
+ 		
+ 		?>
+
+ 	</div>
+
