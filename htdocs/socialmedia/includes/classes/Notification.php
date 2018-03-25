e3216639e239c118af3dd597e3b5f21ab018a05d
kesava <psyku@nottingham.ac.uk>
2018-03-25 18:43:43 +0100

    Added notifications function

--- htdocs/socialmedia/includes/classes/Notification.php
+++ htdocs/socialmedia/includes/classes/Notification.php
@@ -13,5 +13,39 @@ class Notification {
 		$query = mysqli_query($this->con, "SELECT * FROM notifications WHERE viewed='no' AND user_to='$userLoggedIn'");
 		return mysqli_num_rows($query);
 	}
+
+	public function insertNotifcations($post_id, $user_to, $type){
+	
+		$userLoggedIn = $this->user_obj->getUsername();
+		$userLoggedInName= $this->user_obj->getFistAndLastName();
+
+		$date_time = date("Y-m-d H:i:s");
+
+		switch($type) {
+			case 'comment':
+				$message = $userLoggedInName . " commented on your post";
+				break;
+			case 'like':
+				$message = $userLoggedInName . " liked your post";
+				break;
+			case 'profile':
+				$message = $userLoggedInName . " posted on your profile";
+				break;
+			case 'comment_non_owner':
+				$message = $userLoggedInName . " commented on a post you commented on";
+				break;
+			case 'profile_comment':
+				$message = $userLoggedInName . " commented on your profile post";
+				break;
+		
+		}
+
+		$link = "post.php?id=" . $post_id;
+
+		$insert_query = mysqli_query($this->con, "INSERT INTO notifications VALUES('', '$user_to', '$userLoggedIn', '$message', '$link', '$date_time', 'no', 'no')");
+		
+
+
+	}
 }
 ?>
