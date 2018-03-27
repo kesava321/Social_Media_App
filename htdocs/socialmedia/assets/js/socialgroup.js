c4a9552f4165a6352dfd383b01b68808f6c6abf9
kesava <psyku@nottingham.ac.uk>
2018-03-27 12:23:18 +0100

    Added search form

--- htdocs/socialmedia/assets/js/socialgroup.js
+++ htdocs/socialmedia/assets/js/socialgroup.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 
+	$('#search_text_input').focus(function(){
+		if(window.matchMedia("(min-width: 800px)").matches){
+			$(this).animate({width: '250px'}, 500);
+		}
+	});
 
+	$('.button_holder').on('click', function(){
+		document.search_form.submit();
+	})
 	//Button for profile post
 	$('#submit_profile_post').click(function(){
 
