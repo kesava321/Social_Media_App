803aee02ddd5fe2554a5f2eac7b9ee5e6561d7c5
kesava <psyku@nottingham.ac.uk>
2018-03-28 13:20:14 +0100

    Added password update form functionality

--- htdocs/socialmedia/includes/form_handlers/settings_handler.php
+++ htdocs/socialmedia/includes/form_handlers/settings_handler.php
@@ -5,7 +5,7 @@ if(isset($_POST['update_details'])){
 	$last_name = $_POST['last_name'];
 	$email = $_POST['email'];
 
-	$email_check = mysql_query($con, "SELECT * FROM users WHERE email='$email'");
+	$email_check = mysqli_query($con, "SELECT * FROM users WHERE email='$email'");
 	$row = mysqli_fetch_array($email_check);
 	$matched_user = $row['username'];
 
@@ -19,5 +19,45 @@ if(isset($_POST['update_details'])){
 	}
 	else
 		$message = "";
+
+	//////////////////////////////////////////
+
+	if(isset($_POST['update_password'])) {
+
+		$old_password = strip_tags($_POST['old_password']);
+		$new_password_1 = strip_tags($_POST['new_password_1']);
+		$new_password_2 = strip_tags($_POST['new_password_2']);
+
+		$password_query = mysqli_query($con, "SELECT password FROM users WHERE username='$userLoggedIn'");
+		$row = mysqli_fetch_array($password_query);
+		$db_password = $row['password'];
+
+		if(md5($old_password) == $db_password) {
+
+			if($new_password_1 == $new_password_2) {
+
+				if(strlen($new_password_1) <= 4) {
+					$password_message = "Sorry your password must be greater than 4 characters<br><br>";
+				}
+				else {
+					$new_password_md5 = md5($new_password_1);
+					$password_query = mysqli_query($con, "UPDATE users SET password='$new_password_md5' WHERE username='$userLoggedIn'");
+					$password_message = "Password has been changed<br><br>";
+				}
+
+			}
+			else {
+				$password_message = "Your two new passwords need to match<br><br>";
+			}
+		}
+
+		else {
+				$password_message = "The old password is incorrect<br><br>";
+			}
+
+	}
+	else {
+		$password_message = "";
+	}
 
 ?>
--- htdocs/socialmedia/settings.php
+++ htdocs/socialmedia/settings.php
@@ -1,6 +1,6 @@
 <?php
 include("includes/header.php");
-//include("includes/settings_handler.php");
+include("includes/form_handlers/settings_handler.php");
 
 ?>
 
@@ -15,10 +15,21 @@ include("includes/header.php");
 
 	Modify the values and click 'Update Details'
 
+	<?php
+	$user_data_query = mysqli_query($con, "SELECT first_name, last_name, email FROM users WHERE username='$userLoggedIn'");
+	$row = mysqli_fetch_array($user_data_query);
+	$first_name = $row['first_name'];
+	$last_name = $row['last_name'];
+	$email = $row['email'];
+	?>
+
 	<form action="settings.php" method="POST">
-		First Name: <input type="text" name="first_name" value="<?php echo $user['first_name']; ?>"><br>
-		Last Name: <input type="text" name="last_name" value="<?php echo $user['last_name']; ?>"><br>
-		Email: <input type="text" name="email" value="<?php echo $user['email']; ?>"><br>
+		First Name: <input type="text" name="first_name" value="<?php echo $first_name; ?>"><br>
+		Last Name: <input type="text" name="last_name" value="<?php echo $last_name; ?>"><br>
+		Email: <input type="text" name="email" value="<?php echo $email; ?>"><br>
+
+		<?php echo $message; ?>
+		<input type="submit" name="update_details" id="save_details" value="Update Details"><br>
 	</form>
 
 
@@ -27,6 +38,9 @@ include("includes/header.php");
 		Old Password: <input type="password" name="old_password"><br>
 		New Password: <input type="password" name="new_password_1"><br>
 		New Password: <input type="password" name="new_password_2"><br>
+
+		<?php echo $password_message; ?>
+		<input type="submit" name="update_password" id="save_details" value="Update Password"><br>
 	</form>
 
 	<h4>Close Account</h4>
